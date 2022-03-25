2cae8b98e4cb73d591642be208a85dc080f3943d
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-03-25 23:23:52 +0500

    app done

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,59 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import styles from './App.module.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Welcome from './components/Welcome/Welcome';
-import axios from 'axios';
-
-function App() {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    axios.get('http://localhost:3001/api/hello').then((res) => {
-      console.log(res.data);
-    });
-  }, []);
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.scss";
+import { Contact, Home, Portfolio, ProjectDetails } from "./screens";
+import { ContactSection, Footer, Header } from "./components";
+import { CloudinaryContext } from "cloudinary-react";
+import info from "./assets/info.json";
+import TestimonialFormSection from "./components/TestimonialFormSection";
+function Routing({ info }) {
   return (
-    <BrowserRouter>
-      <div className={styles.App}>
-        <header className={styles['App-header']}>
-          <img src={logo} className={styles['App-logo']} alt="logo" />
-          <Welcome />
-          <p>
-            <button onClick={() => setCount((count) => count + 1)}>
-              count is: {count}
-            </button>
-          </p>
-          <p>
-            Edit <code>App.jsx</code> and save to test HMR updates.
-          </p>
-          <p>
-            <a
-              className={styles['App-link']}
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-            {' | '}
-            <a
-              className={styles['App-link']}
-              href="https://vitejs.dev/guide/features.html"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Vite Docs
-            </a>
-          </p>
-          <Routes>
-            <Route path="/about" element={<main>About</main>} />
-            <Route path="/" element={<main>Home</main>} />
-          </Routes>
-        </header>
-      </div>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home info={info} />} />
+      <Route path="/portfolio" element={<Portfolio />} />
+      <Route path="/project-detail" element={<ProjectDetails />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/write-testimonial" element={<TestimonialFormSection />} />
+    </Routes>
   );
 }
-
-export default App;
+export default function App() {
+  return (
+    <CloudinaryContext cloudName="mehfoozurrehman">
+      <Header info={info} />
+      <Routing info={info} />
+      <ContactSection />
+      <Footer />
+    </CloudinaryContext>
+  );
+}
